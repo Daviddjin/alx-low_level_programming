bba316154adf632679f186bae68ccf040d57a9e5
daviddjin <djindavid9@gmail.com>
2023-08-15 18:58:38 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,12 @@
  *
  * Return: Always 0
  */
-void print_alphabet(void);
+
+void print_alphabet(void)
 {
 	char i;
 
-	for (i = 'a'; i <= 'z'i; i++)
-	{
+	for (i = 'a'; i <= 'z' ;  i++)
 		_putchar(i);
-	}
-	_purchar('\n');
+	_putchar('\n');
 }
--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,13 +1,12 @@
-#ifndef MAIN.H
-#define MAIN.H 
+#include "main.h"
 
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     */
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	    print_alphabet();
-	        return (0);
+    print_alphabet();
+    return (0);
 }
