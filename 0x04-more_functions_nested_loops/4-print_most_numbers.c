5fc56482195a66ea2867da214523e1feb5f974b0
daviddjin <djindavid9@gmail.com>
2023-08-17 14:12:36 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,7 +4,6 @@
  * print_most_numbers - print 0 to 9 excluding 2 and 4
  * Return: void
  */
-
 void print_most_numbers(void)
 {
 	int i;
@@ -13,6 +12,6 @@ void print_most_numbers(void)
 	{
 		if (i != 2 && i != 4)
 			_putchar(i + '0');
-	}	
+	}
 	_putchar('\n');
 }
