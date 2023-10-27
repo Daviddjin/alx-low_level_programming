a27bf2c7370dbbc0b297d0135592f36f4c4749d3
Daviddjin <djindavid9@gmail.com>
2023-10-26 19:31:01 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary opposite number
+ * @n: print binary
+ */
+void print_binary(unsigned long int n)
+{
+	int i, count = 0;
+	unsigned long int curr;
+
+	for (i = 63; i >= 0; i--)
+	{
+		curr = n >> i;
+
+		if (curr & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
+
+
+
+
+
+
+
